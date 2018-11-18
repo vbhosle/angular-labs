85b714095011683932e9ecfe5dad7f90ee25661e
vbhosle <bhosleviraj@gmail.com>
2018-11-18 13:08:38 +0530

    Codecraft Custome Directives: Input and Configuration

--- src/root/codecraft/custom-directives/card-hover.directive.ts
+++ src/root/codecraft/custom-directives/card-hover.directive.ts
@@ -1,9 +1,13 @@
-import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from "@angular/core";
+import { Directive, ElementRef, Renderer2, HostListener, HostBinding, Input } from "@angular/core";
 
 @Directive({
     selector: '[ccCardHover]'
 })
 export class CardHoverDirective{
+    @Input('ccCardHover') private config = {
+        querySelector: '.card-text'
+    }
+
     @HostBinding('class.panel-primary') private isHovering: boolean;
 
     constructor(private elementRef: ElementRef,private renderer: Renderer2){
@@ -11,13 +15,13 @@ export class CardHoverDirective{
     }
 
     @HostListener('mouseover') onMouseOver(){
-        let part = this.elementRef.nativeElement.querySelector('.card-text');
+        let part = this.elementRef.nativeElement.querySelector(this.config.querySelector);
         this.renderer.setStyle(part, 'display', 'block');
         this.isHovering = true;
     }
 
     @HostListener('mouseout') onMouseOut(){
-        let part = this.elementRef.nativeElement.querySelector('.card-text');
+        let part = this.elementRef.nativeElement.querySelector(this.config.querySelector);
         this.renderer.setStyle(part, 'display', 'none');
         this.isHovering = false;
     }
--- src/root/codecraft/custom-directives/cd-joke.component.ts
+++ src/root/codecraft/custom-directives/cd-joke.component.ts
@@ -4,7 +4,7 @@ import { Joke } from "../jokes/joke.model";
 @Component({
     selector: 'cd-joke',
     template: `
-    <div class="panel panel-default" ccCardHover>
+    <div class="panel panel-default" [ccCardHover] = "{querySelector: '.card-text'}">
         <h4 class="panel-heading">
             <ng-content select=".setup"></ng-content>
         </h4>
